b6c91a0142fcebe44bc6a8ade790216fc498bfd2
shaunprice <shaundnz@gmail.com>
2024-06-02 13:04:02 +1200

    Test deploy of sample api and lambda

--- lib/made-for-all-api-stack.ts
+++ lib/made-for-all-api-stack.ts
@@ -1,5 +1,5 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class MadeForAllApiStack extends cdk.Stack {
@@ -12,5 +12,27 @@ export class MadeForAllApiStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'MadeForAllApiQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    const sampleLambda = new cdk.aws_lambda_nodejs.NodejsFunction(
+      this,
+      "sample-lambda",
+      {
+        timeout: cdk.Duration.seconds(15),
+        memorySize: 128,
+        entry: "src/lambdas/sample-lambda/index.ts",
+      }
+    );
+
+    const api = new cdk.aws_apigateway.RestApi(this, "made-for-all-api", {
+      defaultCorsPreflightOptions: {
+        allowOrigins: ["http://127.0.0.1:5173", "http://localhost:5173"],
+      },
+    });
+
+    const test = api.root.addResource("test");
+    test.addMethod(
+      "GET",
+      new cdk.aws_apigateway.LambdaIntegration(sampleLambda)
+    );
   }
 }
