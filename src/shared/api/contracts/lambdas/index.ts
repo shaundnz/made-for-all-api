e173b5ba6a7b6fb3a68d9e3bbfb2cacff167cf10
shaunprice <shaundnz@gmail.com>
2024-06-10 19:31:59 +1200

    Added new contracts

--- src/shared/api/contracts/lambdas/index.ts
+++ src/shared/api/contracts/lambdas/index.ts
@@ -1,1 +1,7 @@
+export * from "./GetAllTrackedPlaylistResponseDto";
 export * from "./GetTrackedPlaylistResponse";
+export * from "./CreateTrackedPlaylistRequestDto";
+export * from "./CreateTrackedPlaylistResponseDto";
+export * from "./UpdateTrackedPlaylistRequestDto";
+export * from "./UpdateTrackedPlaylistResponseDto";
+export * from "./DeleteTrackedPlaylistResponseDto";
