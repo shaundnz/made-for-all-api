33a575423fdac5086f287daead6a903123a1e848
shaunprice <shaundnz@gmail.com>
2024-06-17 17:01:06 +1200

    Updated ts global namespace

---
+++ environment.d.ts
@@ -0,0 +1,14 @@
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            [key: string]: string | undefined;
+            DYNAMO_TABLE_NAME: string;
+            DYNAMO_ENDPOINT: string;
+            SPOTIFY_CLIENT_ID: string;
+            SPOTIFY_CLIENT_SECRET: string;
+            SPOTIFY_MADE_FOR_ALL_USER_REFRESH_TOKEN: string;
+        }
+    }
+}
+
+export {};
