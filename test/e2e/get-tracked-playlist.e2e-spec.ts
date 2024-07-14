533612d4317417973b7a7418fcaa735fff666bc1
shaunprice <shaundnz@gmail.com>
2024-07-14 13:01:47 +1200

    Fixed test

--- test/e2e/get-tracked-playlist.e2e-spec.ts
+++ test/e2e/get-tracked-playlist.e2e-spec.ts
@@ -45,7 +45,7 @@ describe("GET /playlists/:id", () => {
         expect(response.body.madeForAllPlaylist).toBeDefined();
         expect(response.body.madeForAllPlaylist.id).toBeDefined();
         expect(response.body.madeForAllPlaylist.name).toBe(
-            "Made For All - Givin It Up Radio"
+            "MadeForAll - Givin It Up Radio"
         );
     });
 
